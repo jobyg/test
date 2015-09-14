c9e53a2f2238c71745f32da66ea2339cbf53ec80
jobyg <gjobycontact@gmail.com>
2015-09-13 22:26:57 -0700

    step4

--- add.rb
+++ add.rb
@@ -3,8 +3,13 @@ input_string = gets;
 orig_string = input_string;
 input_string.chop!
 input_string.delete! '"'
-input_string = input_string.gsub(/\\n/,',')
-input_array = input_string.split(",").map(&:to_i);
+delimiter = ',';
+if input_string.match(/\/\//)
+   delimiter = input_string[2..2]
+   input_string = input_string[5..-1]
+end
+input_string = input_string.gsub(/\\n/,delimiter)
+input_array = input_string.split(delimiter).map(&:to_i);
 sum = 0 ;
 for number in input_array
     sum += number;
