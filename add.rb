025a1b110c2fdb23eb036ccf3bae30cf71420621
jobyg <gjobycontact@gmail.com>
2015-09-13 22:26:15 -0700

    step3

--- add.rb
+++ add.rb
@@ -3,10 +3,11 @@ input_string = gets;
 orig_string = input_string;
 input_string.chop!
 input_string.delete! '"'
+input_string = input_string.gsub(/\\n/,',')
 input_array = input_string.split(",").map(&:to_i);
 sum = 0 ;
 for number in input_array
-   sum += number;
+    sum += number;
 end
 print "input string:" ,orig_string ,"\n"
 print "sum:" ,sum ,"\n"
